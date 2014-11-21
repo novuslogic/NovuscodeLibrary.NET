fe6064b77366cfc057efee1f1276fb3e5d5fae2d
novuslogic <support@novuslogic.com>
2014-11-21 12:15:15 +1100

    Fix issue with DiffYears

--- Source/NovusCodeLibrary4.Utils/DateTimeUtils.cs
+++ Source/NovusCodeLibrary4.Utils/DateTimeUtils.cs
@@ -18,9 +18,20 @@ namespace NovusCodeLibrary.Utils
 
         public static int DiffYears(DateTime aDateTime2, DateTime aDateTime1)
         {
-            DateTime zeroTime = new DateTime(1, 1, 1);
-            TimeSpan span = aDateTime2 - aDateTime1;        
-            int years = (zeroTime + span).Year - 1;        
+            int years = -1;
+
+            if ((aDateTime2 != null) && (aDateTime1 != null))
+            {
+
+                if (aDateTime1 <= aDateTime2)
+                {
+
+                    DateTime zeroTime = new DateTime(1, 1, 1);
+                    TimeSpan span = aDateTime2 - aDateTime1;
+                    years = (zeroTime + span).Year - 1;
+                }
+
+            }
 
             return years;
         }

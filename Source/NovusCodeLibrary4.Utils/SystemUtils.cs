77f1e96200bf8bf68cf3c7ee77f7ffbfe0f6c367
ajohnston <ajohnston@AIMS53-1>
2016-12-07 16:51:27 +1100

    Update 20161207-2

--- Source/NovusCodeLibrary4.Utils/SystemUtils.cs
+++ Source/NovusCodeLibrary4.Utils/SystemUtils.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace NovusCodeLibrary.Utils
 {
@@ -11,6 +12,25 @@ namespace NovusCodeLibrary.Utils
         {
             System.IO.Directory.CreateDirectory(aDirectory);
         }
+
+
+
+        public static string TrailingBackSlash(string aPath)
+        {
+            string separator1 = Path.DirectorySeparatorChar.ToString();
+            string separator2 = Path.AltDirectorySeparatorChar.ToString();
+
+            aPath = aPath.TrimEnd();
+
+            if (aPath.EndsWith(separator1) || aPath.EndsWith(separator2))
+                return aPath;
+                        
+            if (aPath.Contains(separator2))
+                return aPath + separator2;
+
+            return aPath + separator1;
+        }
+
 
     }
 }

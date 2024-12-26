a1491f6730013f9fec1f58c238a08f06df6bedc0
Adam Craig Johnston <adam@novuslogic.com>
2024-12-26 23:15:22 +1100

    Update Version in Packages

---
+++ build.ps1
@@ -0,0 +1,13 @@
+
+$modulePath = "D:\Projects\PSNovusTools\PSNovusTools"
+Import-Module -Name $modulePath -Force
+
+Set-VSPackageVersion -ProjectFile "D:\Projects\NovuscodeLibrary.NET\Source\NovusCodeLibrary.SimpleTemplate\NovusCodeLibrary.SimpleTemplate.csproj" -Version "0.3.0"
+
+Set-VSPackageVersion -ProjectFile "D:\Projects\NovuscodeLibrary.NET\Source\NovusCodeLibrary.JSONUtils\NovusCodeLibrary.JSONUtils.csproj" -Version "0.3.0"
+
+Set-VSPackageVersion -ProjectFile "D:\Projects\NovuscodeLibrary.NET\Source\NovusCodeLibrary.Exceptions\NovusCodeLibrary.Exceptions.csproj" -Version "0.3.0" 
+
+Set-VSPackageVersion -ProjectFile "D:\Projects\NovuscodeLibrary.NET\Source\NovusCodeLibrary.WebUtils\NovusCodeLibrary.WebUtils.csproj" -Version "0.3.0" 
+
+Set-VSPackageVersion -ProjectFile "D:\Projects\NovuscodeLibrary.NET\Source\NovusCodeLibrary.Utils\NovusCodeLibrary.Utils.csproj" -Version "0.3.0"
